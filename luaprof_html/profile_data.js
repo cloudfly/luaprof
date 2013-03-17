b6782b1f19d2d3b62017cf72f8bdab76c1bbb8c9
cloudfly <chenyunfeihappy@gmail.com>
2013-03-17 17:06:56 +0800

    complete data present on web page, having table, pie chart , flow chart

--- luaprof_html/profile_data.js
+++ luaprof_html/profile_data.js
@@ -1,9 +1,9 @@
 var profile_data = [
-{name:'main()', source:'(null)', type:'(null)', line:-1, count:1, total:52, time:9 },
-{name:'test2', source:'/home/cloudfly/www/test.lua', type:'Lua', line:19, count:1, total:8, time:8 },
-{name:'test1', source:'/home/cloudfly/www/test.lua', type:'Lua', line:13, count:1, total:14, time:14 },
-{name:'fibo', source:'/home/cloudfly/www/test.lua', type:'Lua', line:4, count:1, total:21, time:4 },
-{name:'fibo@1', source:'/home/cloudfly/www/test.lua', type:'Lua', line:4, count:2, total:17, time:8 },
-{name:'fibo@2', source:'/home/cloudfly/www/test.lua', type:'Lua', line:4, count:4, total:10, time:8 },
-{name:'fibo@3', source:'/home/cloudfly/www/test.lua', type:'Lua', line:4, count:2, total:2, time:2 },
+{name:'main()', source:'(null)', type:'(null)', line:-1, count:1, total:166, time:37 },
+{name:'test2', source:'test.lua', type:'Lua', line:19, count:1, total:13, time:13 },
+{name:'test1', source:'test.lua', type:'Lua', line:13, count:1, total:52, time:52 },
+{name:'fibo', source:'test.lua', type:'Lua', line:4, count:1, total:65, time:4 },
+{name:'fibo@1', source:'test.lua', type:'Lua', line:4, count:2, total:61, time:45 },
+{name:'fibo@2', source:'test.lua', type:'Lua', line:4, count:4, total:18, time:15 },
+{name:'fibo@3', source:'test.lua', type:'Lua', line:4, count:2, total:3, time:3 },
 ];
